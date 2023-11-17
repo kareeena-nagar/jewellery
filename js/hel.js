822a3c5e2e3bb4802e7233234dacf9e171bcc7f8
kareeena-nagar <150219520+kareeena-nagar@users.noreply.github.com>
2023-11-17 19:25:16 +0530

    initial commit

---
+++ js/hel.js
@@ -0,0 +1,12 @@
+let btn=document.getElementById('toggle-btn') ;
+btn.onclick=function(){
+    document.body.classList.toggle('dark-mode')
+}
+let bt=document.getElementById('toggle-bt') ;
+bt.onclick=function(){
+document.body.classList.toggle('pink-mode')
+}
+let button=document.getElementById('toggle-buttn') ;
+button.onclick=function(){
+   document.body.classList.toggle('blue-mode')
+}
